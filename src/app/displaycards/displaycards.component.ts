1ecc22e781b8cdb1096e2685cb7ef89189828d84
Swetha <swe54cit@gmail.com>
2020-05-08 20:25:21 +0530

    third

--- src/app/displaycards/displaycards.component.ts
+++ src/app/displaycards/displaycards.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { SearchService } from "../search.service";
 @Component({
   selector: "app-displaycards",
   templateUrl: "./displaycards.component.html",
   styleUrls: [ "./displaycards.component.css" ]
 })
-export class DisplaycardsComponent implements OnInit {
+export class DisplaycardsComponent implements OnInit, OnDestroy {
   sreslt = [];
 
   constructor(private s: SearchService) {
     this.sreslt = this.s.imageresults;
     console.log(this.sreslt);
-    this.s.imageresults = [];
   }
 
   ngOnInit() {}
+
+  ngOnDestroy() {
+    this.sreslt = [];
+  }
 }
